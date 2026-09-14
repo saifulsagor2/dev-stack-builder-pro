2c7b625a68bed157ba93efff002795dcd8042e99
saifulsagor2 <saifulsagorofficial@gmail.com>
2026-09-14 11:09:20 +0600

    Fixed all bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
+import { useState } from "react";
+import type { Technology } from "./types/technology";
+
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Technologies from "./components/Technologies/Technologies";
+import YourStack from "./components/YourStack/YourStack";
 
 function App() {
+  const [stack, setStack] = useState<Technology[]>([]);
+
+  const handleAddToStack = (technology: Technology) => {
+    setStack([...stack, technology]);
+  };
+
+  const handleRemove = (id: string) => {
+    const remainingTechnologies = stack.filter(
+      (technology) => technology.id !== id
+    );
+
+    setStack(remainingTechnologies);
+  };
+
+  const handleRemoveAll = () => {
+    setStack([]);
+  };
+
   return (
     <div>
       <Navbar />
+
       <Hero />
-      <Technologies />
+
+      <div className="main-content">
+        <Technologies
+          stack={stack}
+          onAddToStack={handleAddToStack}
+        />
+
+        <YourStack
+          stack={stack}
+          onRemove={handleRemove}
+          onRemoveAll={handleRemoveAll}
+        />
+      </div>
     </div>
   );
 }
